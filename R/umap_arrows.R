608a86fecd9293433f009eef3e59f40d84a3807e
Findlay Bewicke-Copley <findlaycopley@hotmail.co.uk>
2023-03-07 13:28:53 +0000

    Initial commit

---
+++ R/umap_arrows.R
@@ -0,0 +1,8 @@
+#' @title umap_arrows
+#' @param TODO
+#' @keywords UMAP dimplot
+#' @export
+#' @examples
+#' umap_arrows(plot)
+
+umap_arrows()
---
+++ README.md
@@ -0,0 +1,2 @@
+# OkosunLab
+  An R package containing common functions and classes used in the analysis of NGS data in the Okosun lab
