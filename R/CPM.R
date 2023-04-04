b0eaf08059a27604c458d72a7c9a185daf10c108
Findlay Bewicke-Copley <findlaycopley@hotmail.co.uk>
2023-04-04 10:14:16 +0100

    Fixed TPM

--- R/CPM.R
+++ R/CPM.R
@@ -34,38 +34,3 @@ CPM <- function(matrix, log = FALSE, pseudocount = FALSE) {
     ## return cpm
     cpm
 }
-
-
-#' A function to generate tpm from expression matrix
-#'
-#'
-#' @title TPM
-#' @param matrix a matrix of counts, rows for features and columns for samples
-#' @param length a vector of gene lengths in the same order as the matrix
-#' @param log boolean whether to log2 transform the cpm
-#' @param pseudocount boolean whether to add a pseudocount of 1 to the matrix
-#' @return a normalised count matrix
-#' @keywords normalisation tpm expression
-#' @export
-#' @examples
-#' TPM(matrix, lengths)
-#'
-
-TPM <- function(matrix, length, log = TRUE, pseudocount = TRUE) {
-    ## You can't log with zeroes so if you have zeroes you can add a pseudocount of 1 here
-    if ( pseudocount ) {
-        matrix <- matrix + 1
-    }
-    ## reads / gene length
-    rpk <- matrix/length
-    ## scaling factors
-    sf <- colSums(rpk)/(1*10^6)
-    ## divide rpk by sf
-    ## returns feature as column and sample as row so transpose
-    tpm <- apply(rpk, 1, function(col) {col/sf})
-    ## returns feature as column and sample as row so transpose
-    tpm <- t(tpm)
-    if (log) {
-        log2(tpm)
-    }
-}
